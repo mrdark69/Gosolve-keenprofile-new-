ba05948bfa32d5a9c80379aebe36c5081090efac
MrDarkman <mrdark6996@gmail.com>
2017-09-15 23:54:49 +0700

    html and pdf report 1

--- doc_template/gulpfile.js
+++ doc_template/gulpfile.js
@@ -1,4 +1,3 @@
-
 //... For Gulp ruby Sass
 // โหลด package "gulp" มาใช้ (บรรทัดนี้ต้องใส่เสมอ)
 var gulp = require('gulp');
@@ -14,11 +13,11 @@ var src = {
 }
 
 gulp.task('sass', function() {
-       return sass(src.sass,{compass:true})
-        .on('error', function (err) {
+    return sass(src.sass, { compass: true })
+        .on('error', function(err) {
             console.log(err.message);
         })
-       .pipe(gulp.dest('dev/css')) // เก็บไฟล์ css ไว้ที่โฟลเดอร์ css
+        .pipe(gulp.dest('dev/css')) // เก็บไฟล์ css ไว้ที่โฟลเดอร์ css
 });
 
 //gulp.task('sass', function () {
@@ -48,13 +47,13 @@ var minifyCSS = require('gulp-minify-css');
 
 gulp.task('minify-css', function() {
 
-  gulp.src('./dev/css/**/*.css')
-    .pipe(rename({
-        suffix: ".min",
-        extname: ".css"
-    }))
-    .pipe(minifyCSS({keepBreaks:false}))
-    .pipe(gulp.dest('./assets/css/'))
+    gulp.src('./dev/css/**/*.css')
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".css"
+        }))
+        .pipe(minifyCSS({ keepBreaks: false }))
+        .pipe(gulp.dest('./assets/css/'))
 });
 
 
@@ -63,9 +62,9 @@ var coffee = require('gulp-coffee');
 var gutil = require('gulp-util');
 
 gulp.task('coffee', function() {
-  gulp.src('./dev/coffee/*.coffee')
-    .pipe(coffee({bare: true}).on('error', gutil.log))
-    .pipe(gulp.dest('./dev/js/'))
+    gulp.src('./dev/coffee/*.coffee')
+        .pipe(coffee({ bare: true }).on('error', gutil.log))
+        .pipe(gulp.dest('./dev/js/'))
 });
 
 
@@ -74,13 +73,13 @@ gulp.task('coffee', function() {
 var uglify = require('gulp-uglify');
 
 gulp.task('minify-js', function() {
-  gulp.src('./dev/js/**/*.js')
-    .pipe(rename({
-        suffix: ".min",
-        extname: ".js"
-    }))
-    .pipe(uglify())
-    .pipe(gulp.dest('./assets/js/'))
+    gulp.src('./dev/js/**/*.js')
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".js"
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./assets/js/'))
 });
 
 
@@ -99,10 +98,11 @@ gulp.task('browser-sync', function() {
 
 //,'minify-css','coffee','minify-js'
 // 'sass','minify-css','coffee','minify-js',
-gulp.task('default', ['browser-sync'] ,function() {
+gulp.task('default', ['browser-sync'], function() {
     // ระบุว่า default task ทำอะไร (เว้นไว้ก่อน)
+    gulp.watch(['*.html'], browserSync.reload);
     gulp.watch(['**/*.html'], browserSync.reload);
-     gulp.watch(['**/css/**/*.css'], browserSync.reload);
+    gulp.watch(['**/css/**/*.css'], browserSync.reload);
     gulp.watch(['**/*/js/**/*.js'], browserSync.reload);
     // เมื่อไฟล์ scss มีการเปลี่ยนแปลง ก็จะสั่งให้ task "sass" ทำงาน
     // gulp.watch(src.scss, ['sass']);
