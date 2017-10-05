74eba8cfa85d863ed1e4a1e0929f8b4949f02ea5
MrDarkman <mrdark6996@gmail.com>
2017-10-05 21:44:58 +0700

    JobFitScore

---
+++ App_Code/Model/assessment/Model_JobfitScore.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using MVCDatatableApp;
+using Microsoft.AspNet.Identity;
+using gs_newsletter;
+using System.Web.Providers.Entities;
+using System.Security.Cryptography;
+
+/// <summary>
+/// Summary description for Model_User
+/// </summary>
+/// 
+public class Model_Rule1: BaseModel<Model_Rule1>
+{
+    public int RuleID { get; set; }
+    public string Title { get; set; }
+
+    public int Score { get; set; }
+    public decimal CJRRuleScore1 { get; set; }
+    public decimal CJRRuleScore2 { get; set; }
+    public decimal CJRRuleScore3 { get; set; }
+    public decimal CJRRuleScore4 { get; set; }
+    public decimal CJRRuleScore5 { get; set; }
+
+    public List<Model_Rule1> GetAll()
+    {
+        using(SqlConnection cn = new SqlConnection(this.ConnectionString))
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM JobFitScore_Rule1",cn);
+            cn.Open();
+            return MappingObjectCollectionFromDataReaderByName(ExecuteReader(cmd));
+        }
+    }
+
+    public bool UpdateBulk(List<Model_Rule1> data)
+    {
+        bool ret = false;
+        using (SqlConnection cn = new SqlConnection(this.ConnectionString))
+        {
+            cn.Open();
+            foreach (Model_Rule1 item in data)
+            {
+                SqlCommand cmd = new SqlCommand(@"UPDATE JobFitScore_Rule1 SET Title=@Title,Score=@Score, 
+CJRRuleScore1=@CJRRuleScore1 ,CJRRuleScore2=@CJRRuleScore2,CJRRuleScore3=@CJRRuleScore3,
+CJRRuleScore4=@CJRRuleScore4,CJRRuleScore5=@CJRRuleScore5 WHERE RuleID=@RuleID", cn);
+                cmd.Parameters.Add("@RuleID", SqlDbType.Int).Value = item.RuleID;
+                cmd.Parameters.Add("@Title", SqlDbType.NVarChar).Value = item.Title;
+                cmd.Parameters.Add("@Score", SqlDbType.Int).Value = item.Score;
+                cmd.Parameters.Add("@CJRRuleScore1", SqlDbType.Decimal).Value = item.CJRRuleScore1;
+                cmd.Parameters.Add("@CJRRuleScore2", SqlDbType.Decimal).Value = item.CJRRuleScore2;
+                cmd.Parameters.Add("@CJRRuleScore3", SqlDbType.Decimal).Value = item.CJRRuleScore3;
+                cmd.Parameters.Add("@CJRRuleScore4", SqlDbType.Decimal).Value = item.CJRRuleScore4;
+                cmd.Parameters.Add("@CJRRuleScore5", SqlDbType.Decimal).Value = item.CJRRuleScore5;
+               
+                ret = ExecuteNonQuery(cmd) == 1;
+            }
+        }
+
+        return ret;
+    }
+
+
+}
+public class Model_Rule2 : BaseModel<Model_Rule2>
+{
+    public int RuleID { get; set; }
+    public int Score { get; set; }
+
+    public decimal Value { get; set; }
+    public List<Model_Rule2> GetAll()
+    {
+        using (SqlConnection cn = new SqlConnection(this.ConnectionString))
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM IdealScoreRule", cn);
+            cn.Open();
+            return MappingObjectCollectionFromDataReaderByName(ExecuteReader(cmd));
+        }
+    }
+
+    public bool UpdateBulk(List<Model_Rule2> data)
+    {
+        bool ret = false;
+        using (SqlConnection cn = new SqlConnection(this.ConnectionString))
+        {
+            cn.Open();
+            foreach (Model_Rule2 item in data)
+            {
+                SqlCommand cmd = new SqlCommand("UPDATE IdealScoreRule SET Score=@Score,Value=@Value WHERE RuleID=@RuleID", cn);
+                cmd.Parameters.Add("@RuleID", SqlDbType.Int).Value = item.RuleID;
+                cmd.Parameters.Add("@Score", SqlDbType.Int).Value = item.Score;
+                cmd.Parameters.Add("@Value", SqlDbType.Decimal).Value = item.Value;
+             
+                ret = ExecuteNonQuery(cmd) == 1;
+            }
+        }
+
+        return ret;
+    }
+
+}
+public class Model_Rule3 : BaseModel<Model_Rule3>
+{
+    public int RuleID { get; set; }
+    public int Range_Start { get; set; }
+    public int Range_End { get; set; }
+    public string Description { get; set; }
+
+    public List<Model_Rule3> GetAll()
+    {
+        using (SqlConnection cn = new SqlConnection(this.ConnectionString))
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM JobFitScoreExplanation", cn);
+            cn.Open();
+            return MappingObjectCollectionFromDataReaderByName(ExecuteReader(cmd));
+        }
+    }
+
+    public bool UpdateBulk(List<Model_Rule3> data)
+    {
+        bool ret = false;
+        using (SqlConnection cn = new SqlConnection(this.ConnectionString))
+        {
+            cn.Open();
+            foreach (Model_Rule3 item in data)
+            {
+                SqlCommand cmd = new SqlCommand("UPDATE JobFitScoreExplanation SET Range_Start=@Range_Start,Range_End=@Range_End,Description=@Description WHERE RuleID=@RuleID", cn);
+                cmd.Parameters.Add("@RuleID", SqlDbType.Int).Value = item.RuleID;
+                cmd.Parameters.Add("@Range_Start", SqlDbType.Int).Value = item.Range_Start;
+                cmd.Parameters.Add("@Range_End", SqlDbType.Int).Value = item.Range_End;
+                cmd.Parameters.Add("@Description", SqlDbType.NVarChar).Value = item.Description;
+               
+                ret = ExecuteNonQuery(cmd) == 1;
+            }
+        }
+
+        return ret;
+    }
+
+}
+
+public class Model_Rule4 : BaseModel<Model_Rule4>
+{
+    public int RuleID { get; set; }
+    public int Range_Start { get; set; }
+    public int Range_End { get; set; }
+    public string ValueTop { get; set; }
+    public string ValueOther { get; set; }
+    public string ValueBottom { get; set; }
+
+    public List<Model_Rule4> GetAll()
+    {
+        using (SqlConnection cn = new SqlConnection(this.ConnectionString))
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM JobFitScoreResultRule", cn);
+            cn.Open();
+            return MappingObjectCollectionFromDataReaderByName(ExecuteReader(cmd));
+        }
+    }
+
+    public bool UpdateBulk(List<Model_Rule4> data)
+    {
+        bool ret = false;
+        using (SqlConnection cn = new SqlConnection(this.ConnectionString))
+        {
+            cn.Open();
+            foreach (Model_Rule4 item in data)
+            {
+                SqlCommand cmd = new SqlCommand("UPDATE JobFitScoreResultRule SET Range_Start=@Range_Start,Range_End=@Range_End,ValueTop=@ValueTop ,ValueOther=@ValueOther,ValueBottom=@ValueBottom WHERE RuleID=@RuleID", cn);
+                cmd.Parameters.Add("@RuleID", SqlDbType.Int).Value = item.RuleID;
+                cmd.Parameters.Add("@Range_Start", SqlDbType.Int).Value = item.Range_Start;
+                cmd.Parameters.Add("@Range_End", SqlDbType.Int).Value = item.Range_End;
+
+                cmd.Parameters.Add("@ValueTop", SqlDbType.NVarChar).Value = item.ValueTop;
+                cmd.Parameters.Add("@ValueOther", SqlDbType.NVarChar).Value = item.ValueOther;
+                cmd.Parameters.Add("@ValueBottom", SqlDbType.NVarChar).Value = item.ValueBottom;
+                
+                ret = ExecuteNonQuery(cmd) == 1;
+            }
+        }
+
+        return ret;
+    }
+}
+
---
+++ admin/Content/JobFitScoreContent.aspx.cs
@@ -0,0 +1,498 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _JobFitScoreContent : BasePage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        if (!this.Page.IsPostBack)
+        {
+            Model_Rule1 r1 = new Model_Rule1();
+         
+            List<Model_Rule1> rule1 = r1.GetAll();
+          
+
+            Model_Rule1 r1row1 = rule1.Where(o => o.RuleID == 1).FirstOrDefault();
+
+            txtRuleTitle1.Text = r1row1.Title;
+            txtRuleScore1.Text = r1row1.Score.ToString();
+            txtr1_1.Text = r1row1.CJRRuleScore1.ToString();
+            txtr1_2.Text = r1row1.CJRRuleScore2.ToString();
+            txtr1_3.Text = r1row1.CJRRuleScore3.ToString();
+            txtr1_4.Text = r1row1.CJRRuleScore4.ToString();
+            txtr1_5.Text = r1row1.CJRRuleScore5.ToString();
+
+            Model_Rule1 r1row2 = rule1.Where(o => o.RuleID == 2).FirstOrDefault();
+
+            txtRuleTitle2.Text = r1row2.Title;
+            txtRuleScore2.Text = r1row2.Score.ToString();
+            txtr2_1.Text = r1row2.CJRRuleScore1.ToString();
+            txtr2_2.Text = r1row2.CJRRuleScore2.ToString();
+            txtr2_3.Text = r1row2.CJRRuleScore3.ToString();
+            txtr2_4.Text = r1row2.CJRRuleScore4.ToString();
+            txtr2_5.Text = r1row2.CJRRuleScore5.ToString();
+
+            Model_Rule1 r1row3 = rule1.Where(o => o.RuleID == 3).FirstOrDefault();
+
+            txtRuleTitle3.Text = r1row3.Title;
+            txtRuleScore3.Text = r1row3.Score.ToString();
+            txtr3_1.Text = r1row3.CJRRuleScore1.ToString();
+            txtr3_2.Text = r1row3.CJRRuleScore2.ToString();
+            txtr3_3.Text = r1row3.CJRRuleScore3.ToString();
+            txtr3_4.Text = r1row3.CJRRuleScore4.ToString();
+            txtr3_5.Text = r1row3.CJRRuleScore5.ToString();
+
+            Model_Rule1 r1row4 = rule1.Where(o => o.RuleID == 4).FirstOrDefault();
+
+            txtRuleTitle4.Text = r1row4.Title;
+            txtRuleScore4.Text = r1row4.Score.ToString();
+            txtr4_1.Text = r1row4.CJRRuleScore1.ToString();
+            txtr4_2.Text = r1row4.CJRRuleScore2.ToString();
+            txtr4_3.Text = r1row4.CJRRuleScore3.ToString();
+            txtr4_4.Text = r1row4.CJRRuleScore4.ToString();
+            txtr4_5.Text = r1row4.CJRRuleScore5.ToString();
+
+            Model_Rule1 r1row5 = rule1.Where(o => o.RuleID == 5).FirstOrDefault();
+
+            txtRuleTitle5.Text = r1row5.Title;
+            txtRuleScore5.Text = r1row5.Score.ToString();
+            txtr5_1.Text = r1row5.CJRRuleScore1.ToString();
+            txtr5_2.Text = r1row5.CJRRuleScore2.ToString();
+            txtr5_3.Text = r1row5.CJRRuleScore3.ToString();
+            txtr5_4.Text = r1row5.CJRRuleScore4.ToString();
+            txtr5_5.Text = r1row5.CJRRuleScore5.ToString();
+
+
+            Model_Rule2 r2 = new Model_Rule2();
+            List<Model_Rule2> rule2 = r2.GetAll();
+
+
+            Model_Rule2 r2row1 = rule2.Where(o => o.RuleID == 1).FirstOrDefault();
+
+            ideal_socore_1.Text = r2row1.Score.ToString();
+            ideal_value_1.Text = r2row1.Value.ToString();
+
+            Model_Rule2 r2row2 = rule2.Where(o => o.RuleID == 2).FirstOrDefault();
+
+            ideal_socore_2.Text = r2row2.Score.ToString();
+            ideal_value_2.Text = r2row2.Value.ToString();
+
+            Model_Rule2 r2row3 = rule2.Where(o => o.RuleID == 3).FirstOrDefault();
+
+            ideal_socore_3.Text = r2row3.Score.ToString();
+            ideal_value_3.Text = r2row3.Value.ToString();
+
+            Model_Rule2 r2row4 = rule2.Where(o => o.RuleID == 4).FirstOrDefault();
+
+            ideal_socore_4.Text = r2row4.Score.ToString();
+            ideal_value_4.Text = r2row4.Value.ToString();
+
+            Model_Rule2 r2row5 = rule2.Where(o => o.RuleID == 5).FirstOrDefault();
+
+            ideal_socore_5.Text = r2row5.Score.ToString();
+            ideal_value_5.Text = r2row5.Value.ToString();
+
+
+
+
+            Model_Rule3 r3 = new Model_Rule3();
+            List<Model_Rule3> rule3 = r3.GetAll();
+
+            Model_Rule3 r3row1 = rule3.Where(o => o.RuleID == 1).FirstOrDefault();
+            exp_from_1.Text = r3row1.Range_Start.ToString();
+            exp_to_1.Text = r3row1.Range_End.ToString();
+            exp_des_1.Text = r3row1.Description.ToString();
+
+            Model_Rule3 r3row2 = rule3.Where(o => o.RuleID == 2).FirstOrDefault();
+            exp_from_2.Text = r3row2.Range_Start.ToString();
+            exp_to_2.Text = r3row2.Range_End.ToString();
+            exp_des_2.Text = r3row2.Description.ToString();
+
+            Model_Rule3 r3row3 = rule3.Where(o => o.RuleID == 4).FirstOrDefault();
+            exp_from_3.Text = r3row3.Range_Start.ToString();
+            exp_to_3.Text = r3row3.Range_End.ToString();
+            exp_des_3.Text = r3row3.Description.ToString();
+
+            Model_Rule3 r3row4 = rule3.Where(o => o.RuleID == 5).FirstOrDefault();
+            exp_from_4.Text = r3row4.Range_Start.ToString();
+            exp_to_4.Text = r3row4.Range_End.ToString();
+            exp_des_4.Text = r3row4.Description.ToString();
+
+            Model_Rule3 r3row5 = rule3.Where(o => o.RuleID == 6).FirstOrDefault();
+            exp_from_5.Text = r3row5.Range_Start.ToString();
+            exp_to_5.Text = r3row5.Range_End.ToString();
+            exp_des_5.Text = r3row5.Description.ToString();
+
+            Model_Rule3 r3row6 = rule3.Where(o => o.RuleID == 11).FirstOrDefault();
+            exp_from_6.Text = r3row6.Range_Start.ToString();
+            exp_to_6.Text = r3row6.Range_End.ToString();
+            exp_des_6.Text = r3row6.Description.ToString();
+
+            Model_Rule3 r3row7 = rule3.Where(o => o.RuleID == 14).FirstOrDefault();
+            exp_from_7.Text = r3row7.Range_Start.ToString();
+            exp_to_7.Text = r3row7.Range_End.ToString();
+            exp_des_7.Text = r3row7.Description.ToString();
+
+
+
+
+
+
+            Model_Rule4 r4 = new Model_Rule4();
+            List<Model_Rule4> rule4 = r4.GetAll();
+
+            Model_Rule4 r4row1 = rule4.Where(o => o.RuleID == 1).FirstOrDefault();
+
+            result_from_1.Text = r4row1.Range_Start.ToString();
+            result_to_1.Text = r4row1.Range_End.ToString();
+            result_top7_1.Text = r4row1.ValueTop.ToString();
+            result_other_1.Text = r4row1.ValueOther.ToString();
+            result_bottom4_1.Text = r4row1.ValueBottom.ToString();
+
+            Model_Rule4 r4row2 = rule4.Where(o => o.RuleID == 2).FirstOrDefault();
+
+            result_from_2.Text = r4row2.Range_Start.ToString();
+            result_to_2.Text = r4row2.Range_End.ToString();
+            result_top7_2.Text = r4row2.ValueTop.ToString();
+            result_other_2.Text = r4row2.ValueOther.ToString();
+            result_bottom4_2.Text = r4row2.ValueBottom.ToString();
+
+            Model_Rule4 r4row3 = rule4.Where(o => o.RuleID == 4).FirstOrDefault();
+
+            result_from_3.Text = r4row3.Range_Start.ToString();
+            result_to_3.Text = r4row3.Range_End.ToString();
+            result_top7_3.Text = r4row3.ValueTop.ToString();
+            result_other_3.Text = r4row3.ValueOther.ToString();
+            result_bottom4_3.Text = r4row3.ValueBottom.ToString();
+
+            Model_Rule4 r4row4 = rule4.Where(o => o.RuleID == 5).FirstOrDefault();
+
+            result_from_4.Text = r4row4.Range_Start.ToString();
+            result_to_4.Text = r4row4.Range_End.ToString();
+            result_top7_4.Text = r4row4.ValueTop.ToString();
+            result_other_4.Text = r4row4.ValueOther.ToString();
+            result_bottom4_4.Text = r4row4.ValueBottom.ToString();
+
+
+        }
+
+        if (!string.IsNullOrEmpty(Request.QueryString["tab"]))
+        {
+            string tab = Request.QueryString["tab"];
+
+            switch (tab)
+            {
+                case "2":
+                    tab1.Attributes.Add("aria-expanded", "false");
+                    tab2.Attributes.Add("aria-expanded", "true");
+                    tab3.Attributes.Add("aria-expanded", "false");
+                    tab4.Attributes.Add("aria-expanded", "false");
+
+                    li_tab1.Attributes.Remove("class");
+                    li_tab2.Attributes.Add("class", "active");
+                    li_tab3.Attributes.Remove("class");
+                    li_tab4.Attributes.Remove("class");
+
+                    tab_content1.Visible = false;
+                    tab_content2.Visible = true;
+                    tab_content3.Visible = false;
+                    tab_content4.Visible = false;
+
+                    break;
+                case "3":
+                    tab1.Attributes.Add("aria-expanded", "false");
+                    tab2.Attributes.Add("aria-expanded", "false");
+                    tab3.Attributes.Add("aria-expanded", "true");
+                    tab4.Attributes.Add("aria-expanded", "false");
+
+                    li_tab1.Attributes.Remove("class");
+                    li_tab3.Attributes.Add("class", "active");
+                    li_tab2.Attributes.Remove("class");
+                    li_tab4.Attributes.Remove("class");
+
+                    tab_content1.Visible = false;
+                    tab_content2.Visible = false;
+                    tab_content3.Visible = true;
+                    tab_content4.Visible = false;
+
+                    break;
+                case "4":
+                    tab1.Attributes.Add("aria-expanded", "false");
+                    tab2.Attributes.Add("aria-expanded", "false");
+                    tab3.Attributes.Add("aria-expanded", "false");
+                    tab4.Attributes.Add("aria-expanded", "true");
+
+                    li_tab1.Attributes.Remove("class");
+                    li_tab4.Attributes.Add("class", "active");
+                    li_tab3.Attributes.Remove("class");
+                    li_tab2.Attributes.Remove("class");
+
+                    tab_content1.Visible = false;
+                    tab_content2.Visible = false;
+                    tab_content3.Visible = false;
+                    tab_content4.Visible = true;
+
+                    break;
+
+
+            }
+        }
+        else
+        {
+
+            tab_content1.Visible = true;
+            tab_content2.Visible = false;
+            tab_content3.Visible = false;
+            tab_content4.Visible = false;
+
+            tab1.Attributes.Add("aria-expanded", "true");
+            tab2.Attributes.Add("aria-expanded", "false");
+            tab3.Attributes.Add("aria-expanded", "false");
+            tab4.Attributes.Add("aria-expanded", "false");
+
+            li_tab1.Attributes.Add("class", "active");
+            li_tab2.Attributes.Remove("class");
+
+            li_tab3.Attributes.Remove("class");
+            li_tab4.Attributes.Remove("class");
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+    protected void Unnamed_Click(object sender, EventArgs e)
+    {
+        Model_Rule1 r1 = new Model_Rule1();
+
+        List<Model_Rule1> rule1 = new List<Model_Rule1>();
+
+        rule1.Add(new Model_Rule1
+        {
+            RuleID = 1,
+            Title = txtRuleTitle1.Text,
+            Score = int.Parse(txtRuleScore1.Text),
+            CJRRuleScore1 = decimal.Parse(txtr1_1.Text),
+            CJRRuleScore2 = decimal.Parse(txtr1_2.Text),
+            CJRRuleScore3 = decimal.Parse(txtr1_3.Text),
+            CJRRuleScore4 = decimal.Parse(txtr1_4.Text),
+            CJRRuleScore5 = decimal.Parse(txtr1_5.Text),
+
+        });
+        rule1.Add(new Model_Rule1
+        {
+            RuleID = 2,
+            Title = txtRuleTitle2.Text,
+            Score = int.Parse(txtRuleScore2.Text),
+            CJRRuleScore1 = decimal.Parse(txtr2_1.Text),
+            CJRRuleScore2 = decimal.Parse(txtr2_2.Text),
+            CJRRuleScore3 = decimal.Parse(txtr2_3.Text),
+            CJRRuleScore4 = decimal.Parse(txtr2_4.Text),
+            CJRRuleScore5 = decimal.Parse(txtr2_5.Text),
+
+        });
+        rule1.Add(new Model_Rule1
+        {
+            RuleID = 3,
+            Title = txtRuleTitle3.Text,
+            Score = int.Parse(txtRuleScore3.Text),
+            CJRRuleScore1 = decimal.Parse(txtr3_1.Text),
+            CJRRuleScore2 = decimal.Parse(txtr3_2.Text),
+            CJRRuleScore3 = decimal.Parse(txtr3_3.Text),
+            CJRRuleScore4 = decimal.Parse(txtr3_4.Text),
+            CJRRuleScore5 = decimal.Parse(txtr3_5.Text),
+
+        });
+        rule1.Add(new Model_Rule1
+        {
+            RuleID = 4,
+            Title = txtRuleTitle4.Text,
+            Score = int.Parse(txtRuleScore4.Text),
+            CJRRuleScore1 = decimal.Parse(txtr4_1.Text),
+            CJRRuleScore2 = decimal.Parse(txtr4_2.Text),
+            CJRRuleScore3 = decimal.Parse(txtr4_3.Text),
+            CJRRuleScore4 = decimal.Parse(txtr4_4.Text),
+            CJRRuleScore5 = decimal.Parse(txtr4_5.Text),
+
+        });
+        rule1.Add(new Model_Rule1
+        {
+            RuleID = 5,
+            Title = txtRuleTitle5.Text,
+            Score = int.Parse(txtRuleScore5.Text),
+            CJRRuleScore1 = decimal.Parse(txtr5_1.Text),
+            CJRRuleScore2 = decimal.Parse(txtr5_2.Text),
+            CJRRuleScore3 = decimal.Parse(txtr5_3.Text),
+            CJRRuleScore4 = decimal.Parse(txtr5_4.Text),
+            CJRRuleScore5 = decimal.Parse(txtr5_5.Text),
+
+        });
+
+
+        r1.UpdateBulk(rule1);
+
+
+        Model_Rule2 r2 = new Model_Rule2();
+        List<Model_Rule2> rule2 = new List<Model_Rule2>();
+        rule2.Add(new Model_Rule2 {
+            RuleID= 1,
+            Score = int.Parse(ideal_socore_1.Text),
+            Value = decimal.Parse(ideal_value_1.Text)
+        });
+        rule2.Add(new Model_Rule2
+        {
+            RuleID = 2,
+            Score = int.Parse(ideal_socore_2.Text),
+            Value = decimal.Parse(ideal_value_2.Text)
+        });
+        rule2.Add(new Model_Rule2
+        {
+            RuleID = 3,
+            Score = int.Parse(ideal_socore_3.Text),
+            Value = decimal.Parse(ideal_value_3.Text)
+        });
+        rule2.Add(new Model_Rule2
+        {
+            RuleID = 4,
+            Score = int.Parse(ideal_socore_4.Text),
+            Value = decimal.Parse(ideal_value_4.Text)
+        });
+
+
+        rule2.Add(new Model_Rule2
+        {
+            RuleID = 5,
+            Score = int.Parse(ideal_socore_5.Text),
+            Value = decimal.Parse(ideal_value_5.Text)
+        });
+
+        r2.UpdateBulk(rule2);
+
+        Model_Rule3 r3 = new Model_Rule3();
+        List<Model_Rule3> rule3 = new List<Model_Rule3>();
+
+        rule3.Add(new Model_Rule3 {
+            RuleID = 1,
+            Range_Start = int.Parse(exp_from_1.Text),
+            Range_End = int.Parse(exp_to_1.Text),
+            Description = exp_des_1.Text
+
+        });
+        rule3.Add(new Model_Rule3
+        {
+            RuleID = 2,
+            Range_Start = int.Parse(exp_from_2.Text),
+            Range_End = int.Parse(exp_to_2.Text),
+            Description = exp_des_2.Text
+
+        });
+        rule3.Add(new Model_Rule3
+        {
+            RuleID = 4,
+            Range_Start = int.Parse(exp_from_3.Text),
+            Range_End = int.Parse(exp_to_3.Text),
+            Description = exp_des_3.Text
+
+        });
+        rule3.Add(new Model_Rule3
+        {
+            RuleID = 5,
+            Range_Start = int.Parse(exp_from_4.Text),
+            Range_End = int.Parse(exp_to_4.Text),
+            Description = exp_des_4.Text
+
+        });
+        rule3.Add(new Model_Rule3
+        {
+            RuleID = 6,
+            Range_Start = int.Parse(exp_from_5.Text),
+            Range_End = int.Parse(exp_to_5.Text),
+            Description = exp_des_5.Text
+
+        });
+        rule3.Add(new Model_Rule3
+        {
+            RuleID = 11,
+            Range_Start = int.Parse(exp_from_6.Text),
+            Range_End = int.Parse(exp_to_6.Text),
+            Description = exp_des_6.Text
+
+        });
+        rule3.Add(new Model_Rule3
+        {
+            RuleID = 14,
+            Range_Start = int.Parse(exp_from_7.Text),
+            Range_End = int.Parse(exp_to_7.Text),
+            Description = exp_des_7.Text
+
+        });
+
+
+        r3.UpdateBulk(rule3);
+
+
+        Model_Rule4 r4 = new Model_Rule4();
+        List<Model_Rule4> rule4 = new List<Model_Rule4>();
+
+        rule4.Add(new Model_Rule4 {
+            RuleID = 1,
+            Range_Start = int.Parse(result_from_1.Text),
+            Range_End = int.Parse(result_to_1.Text),
+            ValueTop = result_top7_1.Text,
+            ValueOther = result_other_1.Text,
+            ValueBottom= result_bottom4_1.Text
+
+        });
+        rule4.Add(new Model_Rule4
+        {
+            RuleID = 2,
+            Range_Start = int.Parse(result_from_2.Text),
+            Range_End = int.Parse(result_to_2.Text),
+            ValueTop = result_top7_2.Text,
+            ValueOther = result_other_2.Text,
+            ValueBottom = result_bottom4_2.Text
+
+        });
+        rule4.Add(new Model_Rule4
+        {
+            RuleID = 4,
+            Range_Start = int.Parse(result_from_3.Text),
+            Range_End = int.Parse(result_to_3.Text),
+            ValueTop = result_top7_3.Text,
+            ValueOther = result_other_3.Text,
+            ValueBottom = result_bottom4_3.Text
+
+        });
+        rule4.Add(new Model_Rule4
+        {
+            RuleID = 5,
+            Range_Start = int.Parse(result_from_4.Text),
+            Range_End = int.Parse(result_to_4.Text),
+            ValueTop = result_top7_4.Text,
+            ValueOther = result_other_4.Text,
+            ValueBottom = result_bottom4_4.Text
+
+        });
+
+        r4.UpdateBulk(rule4);
+
+
+        Response.Redirect(Request.Url.ToString());
+    }
+}
